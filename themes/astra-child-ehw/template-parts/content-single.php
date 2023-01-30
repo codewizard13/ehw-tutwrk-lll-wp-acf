26fdbc5279a89d145bf6418282c133e5dc2fb42a
Eric L. Hepperle <codewizard13@gmail.com>
2023-01-30 15:33:31 -0600

    ADD template-parts/content-single.php

---
+++ themes/astra-child-ehw/template-parts/content-single.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Template part for displaying single posts.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Astra
+ * @since 1.0.0
+ */
+
+?>
+
+<?php astra_entry_before(); ?>
+
+<article
+<?php
+		echo astra_attr(
+			'article-single',
+			array(
+				'id'    => 'post-' . get_the_id(),
+				'class' => join( ' ', get_post_class() ),
+			)
+		);
+		?>
+>
+
+	<?php astra_entry_top(); ?>
+
+	<?php astra_entry_content_single(); ?>
+
+	<?php
+	if ( get_field(( 'location'))) {
+		echo '<p class="location">Location: ' . get_field('location') . '</p>';
+	}
+
+	if ( get_field( 'profile_description')) {
+		the_field('profile_description');
+	}
+
+	?>
+
+	<?php astra_entry_bottom(); ?>
+
+</article><!-- #post-## -->
+
+<?php astra_entry_after(); ?>
